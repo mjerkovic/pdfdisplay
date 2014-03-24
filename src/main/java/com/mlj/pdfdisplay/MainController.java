50c4d95219488cb160a666e96aa299d5553eb271
Mark Jerkovic <mjerkovic@aconex.com>
2014-03-25 07:52:10 +1100

    Streaming PDF with new HEAD request to support IE *groan*

--- src/main/java/com/mlj/pdfdisplay/MainController.java
+++ src/main/java/com/mlj/pdfdisplay/MainController.java
@@ -1,12 +1,20 @@
 package com.mlj.pdfdisplay;
 
-import static org.apache.commons.codec.binary.Base64.encodeBase64String;
 import static org.springframework.web.bind.annotation.RequestMethod.GET;
+import static org.springframework.web.bind.annotation.RequestMethod.HEAD;
 
+import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import javax.servlet.http.HttpServletResponse;
 
+import org.apache.commons.codec.binary.Base64;
+import org.apache.commons.io.IOUtils;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -14,15 +22,41 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class MainController {
 
     private final String pdfLocation;
+    private String encodedFileContents;
 
     public MainController(String pdfLocation) {
         this.pdfLocation = pdfLocation;
+        encodedFileContents = Base64.encodeBase64String(readFile());
     }
 
     @RequestMapping(method = GET)
     @ResponseBody
     public String display() {
-        return encodeBase64String(readFile());
+        return "http://192.168.7.137:8080/pdf/tos/1";
+    }
+
+    @RequestMapping(value = "/tos/{id}", method = GET, produces = MediaType.APPLICATION_OCTET_STREAM_VALUE)
+    public void tos(HttpServletResponse response, @PathVariable int id) {
+        streamFileToClent(response);
+    }
+
+    @RequestMapping(value = "/tos/{id}", method = HEAD)
+    @ResponseBody
+    public String contentType(@RequestHeader("User-Agent") String userAgent) {
+        return "application/pdf";
+    }
+
+    private void streamFileToClent(HttpServletResponse response) {
+        InputStream is = null;
+        try {
+            is = new ByteArrayInputStream(Base64.decodeBase64(encodedFileContents));
+            IOUtils.copy(is, response.getOutputStream());
+            response.flushBuffer();
+        } catch (IOException e) {
+            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+        } finally {
+            IOUtils.closeQuietly(is);
+        }
     }
 
     private byte[] readFile() {
